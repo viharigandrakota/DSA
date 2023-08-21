7916d67ced7fe6758e83af8321947baf24794bca
viharigandrakota <35991391+viharigandrakota@users.noreply.github.com>
2023-08-20 22:08:53 -0700

    Update Merge_strings_alternativey_1768.py

--- String_and_Array/Merge_strings_alternativey_1768.py
+++ String_and_Array/Merge_strings_alternativey_1768.py
@@ -33,7 +33,7 @@ merged: a p b q c   d
 
 '''
 
-def mergeAlternately(self, word1: str, word2: str) -> str:
+def mergeAlternately(word1: str, word2: str) -> str:
         return "".join(a+b for a , b in zip(word1, word2)) + word1[len(word2):] + word2[len(word1):]
 
 word1 = "abc"
